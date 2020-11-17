9fe225eff9096d7ef5d672ac67c32b1b46ecc552
SGuille <guillesalvatore@gmail.com>
2020-11-17 08:31:56 -0300

    Mejorando niveles

--- scenes/models/WelcomeHUD.gd
+++ scenes/models/WelcomeHUD.gd
@@ -8,11 +8,13 @@ func _show():
 	$GameName.visible = true
 	$StartGame.visible = true
 	$OptionsButton.visible = true
+	$Controls.visible = true
 
 func _hide():
 	$GameName.visible = false
 	$StartGame.visible = false
 	$OptionsButton.visible = false
+	$Controls.visible = false
 
 func _on_OptionsButton_pressed():
 	_hide()
