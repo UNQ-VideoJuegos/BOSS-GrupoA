0c5235bb84afb61e2f740fce315bc31d42c292c9
gonzabl <gonzabl@live.com.ar>
2020-11-17 21:11:39 -0300

    agregado y corregido niveles
    incluye escena pre final

--- scenes/levels/Level3.gd
+++ scenes/levels/Level3.gd
@@ -5,5 +5,3 @@ func _ready():
 	Global.actual_scene = "res://scenes/levels/Level3.tscn"
 	
 	$doors/Interruptor.connect("triggered", $doors/Door1, "_on_Interruptor_triggered") 
-	$doors/Interruptor2.connect("triggered", $doors/Door6, "_on_Interruptor_triggered")
-	$doors/Interruptor3.connect("triggered", $doors/DoorFinal, "_on_Interruptor_triggered") 
--- scenes/levels/Tutorial.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	Global.actual_scene = "res://scenes/levels/Tutorial.tscn"
-	
-	$interruptores/Interruptor.connect("triggered", $puertas/Door, "_on_Interruptor_triggered") 
-	$interruptores/Interruptor2.connect("triggered", $puertas/Door2, "_on_Interruptor_triggered") 
