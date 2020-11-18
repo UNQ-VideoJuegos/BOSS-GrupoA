98545816255e6f0831686d7777478eda6fe6f871
Oscar <lescanooscarpablo@gmail.com>
2020-11-18 00:50:50 -0300

    nivel nuevo y mejoras

--- scenes/levels/Level0.gd
+++ scenes/levels/Level0.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.actual_scene = "res://scenes/levels/EndTutorialLevel.tscn"
+		Global.actual_scene = "res://scenes/levels/Level0.tscn"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
